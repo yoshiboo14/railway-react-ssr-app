e856d75f65e40a71b8581484d13c3ba32f489027
yoshiboo <yoshikikaitech1414@gmail.com>
2023-12-17 23:34:38 +0900

    ウェザーカードの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">天気予報サイト</header>
+      <div className="weather-card">
+        <h2 className="weather-card_region">地名</h2>
+        <h2 className="weather-card_weather">天気</h2>
+      </div>
+      <div className="buttons">
+        <button className="button">次へ</button>
+        <button className="button">前へ</button>
+      </div>
     </div>
   );
 }
