239dcfdbd09b12d0e80d69c0256491e0602e26eb
yoshiboo <yoshikikaitech1414@gmail.com>
2023-12-23 09:55:59 +0900

    radioで複数の都市を追加、選択された都市を表示できるようにステートで管理

--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,14 @@ const App: React.FC = () => {
   const [tempMax, setTempMax] = useState<number>(); //最高気温
   const [humidity, setHumidity] = useState<number>(); //湿度
 
+  // 選択された都市
+  const [selectedCity, setSelectedCity] = useState<string>("Tokyo, JP");
+
   // 天気情報を取得
   useEffect(() => {
     axios
       .get<WeatherData>(
-        "https://api.openweathermap.org/data/2.5/weather?q=Tokyo,JP&appid=439b5d4b97c24212df08275c993f9d27&lang=ja&units=metric"
+        `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=439b5d4b97c24212df08275c993f9d27&lang=ja&units=metric`
       )
       .then((res) => {
         console.log(res.data);
@@ -39,13 +42,63 @@ const App: React.FC = () => {
         setHumidity(res.data.main.humidity);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [selectedCity]);
+
+  // 都市が選択されたときのonChange
+  const handleCityChange = (selectedCity: string) => {
+    setSelectedCity(selectedCity);
+  };
 
   return (
     <>
       <div className="App">
-        <header className="App-header">天気予報サイト</header>
+        <header className="App-header">天気予報アプリ</header>
         <h1 className="weather-region">{cityName}</h1>
+        <label>
+          <input
+            type="radio"
+            value="Hokkaido, JP"
+            checked={selectedCity === "Hokkaido, JP"}
+            onChange={() => handleCityChange("Hokkaido, JP")}
+          />
+          北海道
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Tokyo, JP"
+            checked={selectedCity === "Tokyo, JP"}
+            onChange={() => handleCityChange("Tokyo, JP")}
+          />
+          東京
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Aichi, JP"
+            checked={selectedCity === "Aichi, JP"}
+            onChange={() => handleCityChange("Aichi, JP")}
+          />
+          愛知
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Osaka, JP"
+            checked={selectedCity === "Osaka, JP"}
+            onChange={() => handleCityChange("Osaka, JP")}
+          />
+          大阪
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Fukuoka Prefecture, JP"
+            checked={selectedCity === "Fukuoka Prefecture, JP"}
+            onChange={() => handleCityChange("Fukuoka Prefecture, JP")}
+          />
+          福岡
+        </label>
         <div className="weather-card">
           <div className="weather-card_info">
             <h2 className="weather-card_weather">天気</h2>
@@ -70,8 +123,8 @@ const App: React.FC = () => {
           </div>
         </div>
         <div className="buttons">
-          <button className="button">次へ</button>
-          <button className="button">前へ</button>
+          <button className="button">3時間後</button>
+          <button className="button">3時間前</button>
         </div>
       </div>
     </>
