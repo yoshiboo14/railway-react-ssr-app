2916c4d1406cb09fc194006ae4154319d818a577
yoshiboo <yoshikikaitech1414@gmail.com>
2023-12-26 17:43:23 +0900

    main.jsのpathを追加

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,10 @@
 const express = require("express"); // expressモジュールを読み込み
+const path = require("path");
 const app = express(); // expressオブジェクトを作成
 const PORT = 9000; // ポート番号を指定
+
+// dist ディレクトリを静的ファイルのルートとして指定
+app.use(express.static(path.join(__dirname, "dist")));
 
 // サーバーを起動
 app.listen(PORT, () => {
