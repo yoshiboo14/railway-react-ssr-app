31fc5cb6f94923a233a4c73a3cf1f762eeab2cfb
yoshiboo <yoshikikaitech1414@gmail.com>
2023-12-26 17:58:19 +0900

    重複している部分を削除してリファクタリング

--- webpack.config.js
+++ webpack.config.js
@@ -5,91 +5,50 @@ const webpack = require("webpack"); // webpackを追加
 const path = require("path");
 
 const config = {
-  mode: "production",
   // エントリーを指定
   entry: "./src/server/index.js",
   // 依存関係解決に参照するファイルの拡張子を指定
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   resolve: {
     extensions: [".js", ".json", ".jsx"],
+    alias: {
+      path: require.resolve("path-browserify"),
+    },
+    fallback: {
+      path: require.resolve("path-browserify"),
+    },
   },
+  target: "node", // こちらを追加
+  mode: "production",
   module: {
     rules: [
       {
-        // 拡張子 js のファイル（正規表現）
-        test: /\.js$/, // 対象の拡張子
-        // ローダーの指定
-        loader: "babel-loader",const path = require("path");
-        const webpack = require("webpack");
-        
-        /** @type {import('webpack').Configuration} */
-        const config = {
-          // エントリーを指定
-          entry: "./src/server/index.js",
-          // 依存関係解決に参照するファイルの拡張子を指定
-          output: {
-            filename: "main.js",
-            path: path.resolve(__dirname, "dist"),
-          },
-          resolve: {
-            extensions: [".js", ".json", ".jsx"],
-            alias: {
-              path: require.resolve("path-browserify"),
-            },
-            fallback: {
-              path: require.resolve("path-browserify"),
-            },
-          },
-          target: "node", // こちらを追加
-          mode: "production",
-          module: {
-            rules: [
-              {
-                test: /\.js$/,
-                loader: "babel-loader",
-              },
-              {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-              },
-            ],
-          },
-          plugins: [
-            new webpack.ProvidePlugin({
-              express: "express",
-            }),
-          ],
-          // Dev Serverの設定
-          devServer: {
-            // contentBase: './dist',  // この行を削除またはコメントアウトするか、staticプロパティを使用する
-            static: {
-              directory: path.join(__dirname, "dist"), // 適切なディレクトリに変更
-            },
-            port: 9000,
-            open: true,
-          },
-        };
-        
-        module.exports = config;
-        
+        test: /\.js$/,
+        loader: "babel-loader",
       },
-      //   {
-      // 拡張子 css のファイル（正規表現）
-      // test: /\.css$/,
-      // use: ["style-loader", "css-loader"],
-      //   },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   plugins: [
-    new webpack.IgnorePlugin({
-      resourceRegExp: /^express$/,
+    new webpack.ProvidePlugin({
+      express: "express",
     }),
   ],
+  // Dev Serverの設定
   devServer: {
+    // contentBase: './dist',  // この行を削除またはコメントアウトするか、staticプロパティを使用する
     static: {
-      directory: "./dist",
+      directory: path.join(__dirname, "dist"), // 適切なディレクトリに変更
     },
+    port: 9000,
+    open: true,
   },
 };
 
-// 設定を CommnJS 形式でエクスポート
 module.exports = config;
