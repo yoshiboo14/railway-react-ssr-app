6c12f9a7bc1e5874a4eb75a2b8703df40ed8cfd7
yoshiboo <yoshikikaitech1414@gmail.com>
2023-12-26 17:36:55 +0900

    main.jsの設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,59 @@ const config = {
         // 拡張子 js のファイル（正規表現）
         test: /\.js$/, // 対象の拡張子
         // ローダーの指定
-        loader: "babel-loader",
+        loader: "babel-loader",const path = require("path");
+        const webpack = require("webpack");
+        
+        /** @type {import('webpack').Configuration} */
+        const config = {
+          // エントリーを指定
+          entry: "./src/server/index.js",
+          // 依存関係解決に参照するファイルの拡張子を指定
+          output: {
+            filename: "main.js",
+            path: path.resolve(__dirname, "dist"),
+          },
+          resolve: {
+            extensions: [".js", ".json", ".jsx"],
+            alias: {
+              path: require.resolve("path-browserify"),
+            },
+            fallback: {
+              path: require.resolve("path-browserify"),
+            },
+          },
+          target: "node", // こちらを追加
+          mode: "production",
+          module: {
+            rules: [
+              {
+                test: /\.js$/,
+                loader: "babel-loader",
+              },
+              {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+              },
+            ],
+          },
+          plugins: [
+            new webpack.ProvidePlugin({
+              express: "express",
+            }),
+          ],
+          // Dev Serverの設定
+          devServer: {
+            // contentBase: './dist',  // この行を削除またはコメントアウトするか、staticプロパティを使用する
+            static: {
+              directory: path.join(__dirname, "dist"), // 適切なディレクトリに変更
+            },
+            port: 9000,
+            open: true,
+          },
+        };
+        
+        module.exports = config;
+        
       },
       //   {
       // 拡張子 css のファイル（正規表現）
