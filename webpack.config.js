c58c46127d041dfd13aace324b9facd1afe56865
yoshiboo <yoshikikaitech1414@gmail.com>
2023-12-29 13:49:05 +0900

    余分なところを削除とcommonjs形式に変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,27 @@
 /** ↓ エディタで補完を効かせるための JSDoc */
 /** @type {import('webpack').Configuration} */
 
-const webpack = require("webpack"); // webpackを追加
 const path = require("path");
 
 const config = {
+  target: "node",
   // エントリーを指定
   entry: "./src/server/index.js",
   // 依存関係解決に参照するファイルの拡張子を指定
+  mode: "development",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
-  resolve: {
-    extensions: [".js", ".json", ".jsx"],
-    alias: {
-      path: require.resolve("path-browserify"),
-    },
-    fallback: {
-      path: require.resolve("path-browserify"),
-    },
-  },
-  target: "node", // こちらを追加
-  mode: "production",
   module: {
     rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         loader: "babel-loader",
       },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
     ],
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      express: "express",
-    }),
-  ],
-  // Dev Serverの設定
-  devServer: {
-    // contentBase: './dist',  // この行を削除またはコメントアウトするか、staticプロパティを使用する
-    static: {
-      directory: path.join(__dirname, "dist"), // 適切なディレクトリに変更
-    },
-    port: 9000,
-    open: true,
   },
 };
 
