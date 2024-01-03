2382d48d6c6ca91cbf80da90792326e0de70fbbe
yoshiboo <yoshikikaitech1414@gmail.com>
2024-01-03 13:51:08 +0900

    ESmodule形式で出力する場合の記述

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,8 @@
+import React from "react";
 import path from "path";
 import express from "express";
+import { renderToString } from "react-dom/server";
+import App from "../App.tsx";
 
 // expressオブジェクトを作成
 const app = express();
@@ -9,12 +12,48 @@ const PORT = 9000;
 // dist ディレクトリを静的ファイルのルートとして指定
 app.use(express.static(path.join(__dirname, "dist")));
 
+app.get("/", (req, res) => {
+  // コンポーネントをHTMLに変換
+  const WeatherApp = renderToString(<App />);
+
+  // HTMLに変換されたAppコンポーネントを埋め込んだHTMLを作成
+  const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>お天気SSR</title>
+        <link rel="stylesheet" type="text/css" href="./styles.css" />
+        <script src="./main.mjs"></script>
+        <script src="./otamesi.js"></script>
+      </head>
+      <body>
+        <div id="ssr-root">${WeatherApp}</div>
+      </body>
+    </html>
+  `;
+  // コンポーネントが埋め込まれたHTMLをレスポンス
+  res.send(html);
+  // console.log(html);
+});
+
 // サーバーを起動
 app.listen(PORT, () => {
   console.log(`ポート${PORT}で待受中...`);
 });
 
+app.get("/main.mjs", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/" + "main.mjs"));
+});
+
+app.get("/otamesi.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/" + "otamesi.js"));
+});
+
+app.get("/styles.css", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/" + "styles.css"));
+});
+
 // ホームページに対してGET要求があった際の処理
-app.get("/", (req, res) => {
+app.get("/aa", (req, res) => {
   res.send("Hello World");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,59 @@
 /** ↓ エディタで補完を効かせるための JSDoc */
 /** @type {import('webpack').Configuration} */
 
-const path = require("path");
+import path from "path";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 const config = {
-  target: "node",
-  // エントリーを指定
-  entry: "./src/server/index.js",
   // 依存関係解決に参照するファイルの拡張子を指定
   mode: "development",
+  // エントリーを指定
+  entry: "./src/server/index.js",
+  target: "node",
+  devtool: "source-map",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "main.mjs",
+    // 出力するフォルダ
+    path: path.resolve(new URL(".", import.meta.url).pathname, "dist"),
     publicPath: "/",
+    // module: true,
   },
+  // experiments: {
+  //   outputModule: true,
+  // },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react", //ReactのPresetを追加
+              "@babel/preset-typescript",
+            ],
+            plugins: ["@babel/plugin-syntax-jsx"], //JSXパース用
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          // CSSファイルを個別に出力するためのローダー
+          MiniCssExtractPlugin.loader,
+          // "style-loader",
+          "css-loader",
+        ],
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
 };
 
-module.exports = config;
+export default config;
