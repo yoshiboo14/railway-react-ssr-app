f8bba705e124f7c63215fa921a4bb4a0e1d10ac3
yoshiboo <yoshikikaitech1414@gmail.com>
2024-01-07 23:00:42 +0900

    クライアント側の設定を追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,39 @@
 /** ↓ エディタで補完を効かせるための JSDoc */
 /** @type {import('webpack').Configuration} */
 
-import path from "path";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+// CommonJS
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const config = {
-  // 依存関係解決に参照するファイルの拡張子を指定
+// ESmodule
+// import path from "path";
+// import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+// クライアント側
+const client = {
+  entry: "/src/client/client.tsx",
   mode: "development",
-  // エントリーを指定
-  entry: "./src/server/index.js",
-  target: "node",
-  devtool: "source-map",
+  devtool: "source-map", // マップを追加
   output: {
-    filename: "main.mjs",
-    // 出力するフォルダ
-    path: path.resolve(new URL(".", import.meta.url).pathname, "dist"),
-    publicPath: "/",
-    // module: true,
+    filename: "client.bundle.js",
+    path: path.resolve(__dirname, "dist"), // 出力するフォルダ
   },
-  // experiments: {
-  //   outputModule: true,
-  // },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/,
+        test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              "@babel/preset-react", //ReactのPresetを追加
-              "@babel/preset-typescript",
-            ],
-            plugins: ["@babel/plugin-syntax-jsx"], //JSXパース用
-          },
+          loader: "ts-loader",
         },
       },
       {
         test: /\.css$/,
         use: [
-          // CSSファイルを個別に出力するためのローダー
-          MiniCssExtractPlugin.loader,
+          MiniCssExtractPlugin.loader, // CSSファイルを個別に出力するためのローダー
           // "style-loader",
           "css-loader",
         ],
@@ -56,4 +47,57 @@ const config = {
   ],
 };
 
-export default config;
+// サーバーサイド側
+const server = {
+  mode: "development",
+  entry: "./src/server/index.js",
+  target: "node",
+  devtool: "source-map", // マップを追加
+  output: {
+    filename: "server.bundle.js",
+    // CommonJS
+    path: path.resolve(__dirname, "dist"), // 出力するフォルダ
+    // ESmodule
+    // path: path.resolve(new URL(".", import.meta.url).pathname, "dist"),
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, // CSSファイルを個別に出力するためのローダー
+          // "style-loader",
+          "css-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+};
+
+// CommonJS
+module.exports = [client, server];
+// ESmodule
+// export default config;
